ff230bba2e91861e3bd5ea1760b6b7547311d375
Jukka Suomela <jukka.suomela@iki.fi>
2023-02-08 20:09:46 +0200

    Better logging, multithreading

--- build.sh
+++ build.sh
@@ -13,6 +13,9 @@ total="$2"
 git submodule init
 git submodule update
 
+mkdir -p "/scratch/$project/type-ratio-data/in"
+mkdir -p "/scratch/$project/type-ratio-log"
+
 cat src/type-ratio-batch |
     sed "s/_PROJECT_/$project/g" |
     sed "s/_TOTAL_/$total/g" > type-ratio-batch
